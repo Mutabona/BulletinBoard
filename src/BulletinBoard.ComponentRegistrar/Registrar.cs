b9857b662c34a97d90c2f4aa83219ad8a16c8ad6
Mutabona <k_dod1@mail.ru>
2024-09-03 11:51:44 +0300

    Add registrar

---
+++ src/BulletinBoard.ComponentRegistrar/Registrar.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BulletinBoard.ComponentRegistrar;
+
+public static class Registrar
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
