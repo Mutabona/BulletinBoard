6b8155c6c28a67ca8e923a43c9d7bb8d74778d71
Mutabona <k_dod1@mail.ru>
2024-09-20 17:58:15 +0300

    Add DbMigrator

---
+++ src/BulletinBoard.DbMigrator/MigrationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulletinBoard.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulletinBoard.DbMigrator;
+
+public class MigrationDbContext : ApplicationDbContext
+{
+    /// <summary>
+    /// Создаёт экземпляр <see cref="MigrationDbContext"/>
+    /// </summary>
+    /// <param name="dbContextOptions"></param>
+    public MigrationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+
+    }
+}
--- src/BulletinBoard.DbMigrator/Migrations/20240920122008_Initial.cs
+++ src/BulletinBoard.DbMigrator/Migrations/20240920122008_Initial.cs
@@ -3,26 +3,46 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace BulletinBoard.DataAccess.Migrations
+namespace BulletinBoard.DbMigrator.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class Initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ParentCategoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categories_Categories_ParentCategoryId",
+                        column: x => x.ParentCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "Users",
                 columns: table => new
                 {
                     Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    Name = table.Column<string>(type: "text", nullable: false),
-                    Surname = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
                     Lastname = table.Column<string>(type: "text", nullable: false),
-                    Email = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
                     Phone = table.Column<string>(type: "text", nullable: false),
-                    Login = table.Column<string>(type: "text", nullable: false),
-                    Password = table.Column<string>(type: "text", nullable: false),
+                    Login = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
                     Role = table.Column<string>(type: "text", nullable: false),
                     CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
                 },
@@ -37,13 +57,20 @@ namespace BulletinBoard.DataAccess.Migrations
                 {
                     Id = table.Column<Guid>(type: "uuid", nullable: false),
                     OwnerId = table.Column<Guid>(type: "uuid", nullable: false),
-                    Title = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
                     Description = table.Column<string>(type: "text", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uuid", nullable: false),
                     CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Bulletins", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bulletins_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
                         name: "FK_Bulletins_Users_OwnerId",
                         column: x => x.OwnerId,
@@ -60,7 +87,7 @@ namespace BulletinBoard.DataAccess.Migrations
                     Content = table.Column<byte[]>(type: "bytea", nullable: false),
                     ContentType = table.Column<string>(type: "text", nullable: false),
                     Length = table.Column<int>(type: "integer", nullable: false),
-                    BulletinId = table.Column<Guid>(type: "uuid", nullable: true),
+                    BulletinId = table.Column<Guid>(type: "uuid", nullable: false),
                     CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
                 },
                 constraints: table =>
@@ -70,13 +97,24 @@ namespace BulletinBoard.DataAccess.Migrations
                         name: "FK_Image_Bulletins_BulletinId",
                         column: x => x.BulletinId,
                         principalTable: "Bulletins",
-                        principalColumn: "Id");
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
                 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bulletins_CategoryId",
+                table: "Bulletins",
+                column: "CategoryId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Bulletins_OwnerId",
                 table: "Bulletins",
                 column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_ParentCategoryId",
+                table: "Categories",
+                column: "ParentCategoryId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Image_BulletinId",
@@ -92,6 +130,9 @@ namespace BulletinBoard.DataAccess.Migrations
 
             migrationBuilder.DropTable(
                 name: "Bulletins");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
 
             migrationBuilder.DropTable(
                 name: "Users");
